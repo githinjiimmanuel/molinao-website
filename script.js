0fa86ccb02219d3f2408bbf525a5f450808753d7
githinjiimmanuel <157451590+githinjiimmanuel@users.noreply.github.com>
2026-02-28 06:00:31 +0300

    Add files via upload
    Initial molinao website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function orderNow() {
+    alert("Call or WhatsApp 0743169485 to place your order!");
+}
+
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Thank you for contacting Molinao Kenya Coffee Shop! We will respond shortly.";
+}
